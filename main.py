c647c39e7fc8600be8b4698ea0cdc8c147d8b498
Yasmim Santos <cavalcantiyasmim213@gmail.com>
2025-10-03 14:47:41 -0300

    add etapa 4- atualizar disponibilidade

--- main.py
+++ main.py
@@ -38,20 +38,53 @@ CREATE TABLE IF NOT EXISTS livros (
 
 # print("-"*50)
 
-def listar_livros():
+# def listar_livros():
+#     try:
+#         conexao = sqlite3.connect("Biblioteca.db")
+#         cursor = conexao.cursor()
+#         cursor.execute("SELECT * FROM livros")
+#         for linha in cursor.fetchall():
+#             print(f"id:{linha[0]}| Titulo: {linha[1]} |Autor:{linha[2]} | Ano:{linha[3]}| Dispolivel:{linha[4]}")
+
+#     except Exception as erro:
+#         print(f"erro ao inserir o livro {erro}")
+#     finally:
+#         if conexao:
+#             conexao.close()
+# listar_livros()
+
+
+def atualizar_disponibilidade():
     try:
         conexao = sqlite3.connect("Biblioteca.db")
         cursor = conexao.cursor()
-        cursor.execute("SELECT * FROM livros")
-        for linha in cursor.fetchall():
-            print(f"id:{linha[0]}| Titulo: {linha[1]} |Autor:{linha[2]} | Ano:{linha[3]}| Dispolivel:{linha[4]}")
+        
+        id_livro = int(input("Digite o ID do livro que deseja attualizar a disponibilidade: ").strip())
+
+        nova_disponibilidade = input("Diggite a nova dsiponibilidade (Sim/Não): ")
+
+        cursor.execute("""
+        UPDATE livros 
+        SET Disponivel = ? 
+        WHERE id = ?""", (nova_disponibilidade, id_livro))
+
+        conexao.commit()
+        if cursor.rowcount>0:
+            print("Disponibilidade atualizada com sucesso!")
+        else:
+            print("Nenhun livro encontado com o ID fornecido.")
 
     except Exception as erro:
-        print(f"erro ao inserir o livro {erro}")
+        print(f"erro ao mudar disponibilidade do livro {erro}")
     finally:
         if conexao:
             conexao.close()
-listar_livros()
+
+atualizar_disponibilidade()
+
+
+
+
 
 
 
