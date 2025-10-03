a986f2fad2829e35b8dfdf0b9a78d39934d31623
Yasmim Santos <cavalcantiyasmim213@gmail.com>
2025-10-03 16:42:08 -0300

    add etapa 6- menu interativo no console

--- main.py
+++ main.py
@@ -15,85 +15,85 @@ CREATE TABLE IF NOT EXISTS livros (
 """)
 
 
-# def inserir_livros (Titulo,Autor,Ano):
-#     try:
-#         conexao = sqlite3.connect("Biblioteca.db")
-#         cursor = conexao.cursor()
-#         cursor.execute("""
-#         INSERT INTO livros (Titulo,Autor,Ano,Disponivel)
-#         VALUES (?,?,?,?)                   
-#         """,
-#         (Titulo,Autor,Ano,"sim")
-#         )
-#         conexao.commit()  
-#     except Exception as erro:
-#         print(f"erro ao inserir o livro {erro}")
-#     finally:
-#         if conexao:
-#             conexao.close()
-# Titulo  = input("Digite o Titulo do livro: ").lower()
-# Autor = input("Digite o Autor do livro: ").lower()
-# Ano = int(input("Digite a Ano do livro: "))
-# inserir_livros(Titulo,Autor,Ano)
+def inserir_livros ():
+    try:
+        conexao = sqlite3.connect("Biblioteca.db")
+        cursor = conexao.cursor()
+        Titulo  = input("Digite o Titulo do livro: ").lower()
+        Autor = input("Digite o Autor do livro: ").lower()
+        Ano = int(input("Digite a Ano do livro: "))
 
-# print("-"*50)
-
-# def listar_livros():
-#     try:
-#         conexao = sqlite3.connect("Biblioteca.db")
-#         cursor = conexao.cursor()
-#         cursor.execute("SELECT * FROM livros")
-#         for linha in cursor.fetchall():
-#             print(f"id:{linha[0]}| Titulo: {linha[1]} |Autor:{linha[2]} | Ano:{linha[3]}| Dispolivel:{linha[4]}")
-
-#     except Exception as erro:
-#         print(f"erro ao inserir o livro {erro}")
-#     finally:
-#         if conexao:
-#             conexao.close()
-# listar_livros()
+        cursor.execute("""
+        INSERT INTO livros (Titulo,Autor,Ano,Disponivel)
+        VALUES (?,?,?,?)                   
+        """,
+        (Titulo,Autor,Ano,"sim")
+        )
+        conexao.commit()  
+    except Exception as erro:
+        print(f"erro ao inserir o livro {erro}")
+    finally:
+        if conexao:
+            conexao.close()
 
 
-# def atualizar_disponibilidade():
-#     try:
-#         conexao = sqlite3.connect("Biblioteca.db")
-#         cursor = conexao.cursor()
+def listar_livros():
+    try:
+        conexao = sqlite3.connect("Biblioteca.db")
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM livros")
+        for linha in cursor.fetchall():
+            print(f"id:{linha[0]}| Titulo: {linha[1]} |Autor:{linha[2]} | Ano:{linha[3]}| Dispolivel:{linha[4]}")
+
+    except Exception as erro:
+        print(f"erro ao inserir o livro {erro}")
+    finally:
+        if conexao:
+            conexao.close()
+
+
+
+def atualizar_disponibilidade():
+    try:
+        conexao = sqlite3.connect("Biblioteca.db")
+        cursor = conexao.cursor()
         
-#         id_livro = int(input("Digite o ID do livro que deseja attualizar a disponibilidade: ").strip())
+        id_livro = int(input("Digite o ID do livro que deseja attualizar a disponibilidade: ").strip())
 
-#         nova_disponibilidade = input("Diggite a nova dsiponibilidade (Sim/Não): ")
+        nova_disponibilidade = input("Diggite a nova dsiponibilidade (Sim/Não): ")
 
-#         cursor.execute("""
-#         UPDATE livros 
-#         SET Disponivel = ? 
-#         WHERE id = ?""", (nova_disponibilidade, id_livro))
+        cursor.execute("""
+        UPDATE livros 
+        SET Disponivel = ? 
+        WHERE id = ?""", (nova_disponibilidade, id_livro))
 
-#         conexao.commit()
-#         if cursor.rowcount>0:
-#             print("Disponibilidade atualizada com sucesso!")
-#         else:
-#             print("Nenhun livro encontado com o ID fornecido.")
+        conexao.commit()
+        if cursor.rowcount>0:
+            print("Disponibilidade atualizada com sucesso!")
+        else:
+            print("Nenhun livro encontado com o ID fornecido.")
 
-#     except Exception as erro:
-#         print(f"erro ao mudar disponibilidade do livro {erro}")
-#     finally:
-#         if conexao:
-#             conexao.close()
-
-# atualizar_disponibilidade()
+    except Exception as erro:
+        print(f"erro ao mudar disponibilidade do livro {erro}")
+    finally:
+        if conexao:
+            conexao.close()
 
 
-def deletar_livro(id_livros):
+
+
+def deletar_livro():
     try:
         conexao = sqlite3.connect("biblioteca.db")
         cursor = conexao.cursor()
+        deletar= int(input("Digite o id do livro que deseja deletar: "))
 
-        cursor.execute("DELETE FROM livros WHERE id = ?", (id_livros,))
+        cursor.execute("DELETE FROM livros WHERE id = ?", (deletar,))
 
         conexao.commit()
 
         if cursor.rowcount > 0:
-            print("Livro foi removido com cusesso ")
+            print("Livro foi removido com susesso ")
         else:
             print("Nenhum livro encontrado")
     except Exception as erro:
@@ -101,10 +101,25 @@ def deletar_livro(id_livros):
     finally:
         if conexao:
             conexao.close()
-deletar= int(input("Digite o id do livro que deseja deletar: "))
-deletar_livro(deletar)
 
 
 
-
-
+def menu():
+    while True:
+        print("\nMenu:")
+        print("1.Cadastra livro")
+        print("2.Listar livrro")
+        print("3.Atualizar disponibiilidade")
+        print("4.Deletar livro")
+        print("5.Sair")
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            inserir_livros ()
+        if opcao == "2":
+            listar_livros()
+        if opcao == "3":
+            atualizar_disponibilidade()
+        if opcao == "4": 
+            deletar_livro()
+menu()
+                
