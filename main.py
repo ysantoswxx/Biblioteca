cca7c357ece5e4e0117d4c7c39bb93f9d688fb51
Yasmim Santos <cavalcantiyasmim213@gmail.com>
2025-10-03 15:02:16 -0300

    ADD etapa 5- deletar os livros

--- main.py
+++ main.py
@@ -54,35 +54,55 @@ CREATE TABLE IF NOT EXISTS livros (
 # listar_livros()
 
 
-def atualizar_disponibilidade():
-    try:
-        conexao = sqlite3.connect("Biblioteca.db")
-        cursor = conexao.cursor()
+# def atualizar_disponibilidade():
+#     try:
+#         conexao = sqlite3.connect("Biblioteca.db")
+#         cursor = conexao.cursor()
         
-        id_livro = int(input("Digite o ID do livro que deseja attualizar a disponibilidade: ").strip())
+#         id_livro = int(input("Digite o ID do livro que deseja attualizar a disponibilidade: ").strip())
 
-        nova_disponibilidade = input("Diggite a nova dsiponibilidade (Sim/Não): ")
+#         nova_disponibilidade = input("Diggite a nova dsiponibilidade (Sim/Não): ")
 
-        cursor.execute("""
-        UPDATE livros 
-        SET Disponivel = ? 
-        WHERE id = ?""", (nova_disponibilidade, id_livro))
+#         cursor.execute("""
+#         UPDATE livros 
+#         SET Disponivel = ? 
+#         WHERE id = ?""", (nova_disponibilidade, id_livro))
+
+#         conexao.commit()
+#         if cursor.rowcount>0:
+#             print("Disponibilidade atualizada com sucesso!")
+#         else:
+#             print("Nenhun livro encontado com o ID fornecido.")
+
+#     except Exception as erro:
+#         print(f"erro ao mudar disponibilidade do livro {erro}")
+#     finally:
+#         if conexao:
+#             conexao.close()
+
+# atualizar_disponibilidade()
+
+
+def deletar_livro(id_livros):
+    try:
+        conexao = sqlite3.connect("biblioteca.db")
+        cursor = conexao.cursor()
+
+        cursor.execute("DELETE FROM livros WHERE id = ?", (id_livros,))
 
         conexao.commit()
-        if cursor.rowcount>0:
-            print("Disponibilidade atualizada com sucesso!")
-        else:
-            print("Nenhun livro encontado com o ID fornecido.")
 
+        if cursor.rowcount > 0:
+            print("Livro foi removido com cusesso ")
+        else:
+            print("Nenhum livro encontrado")
     except Exception as erro:
-        print(f"erro ao mudar disponibilidade do livro {erro}")
+        print(f"erro ao tentar excluir livro {erro} ")
     finally:
         if conexao:
             conexao.close()
-
-atualizar_disponibilidade()
-
-
+deletar= int(input("Digite o id do livro que deseja deletar: "))
+deletar_livro(deletar)
 
 
 
