1226f9c7840c1774324001650bc557af2e533014
Yasmim Santos <cavalcantiyasmim213@gmail.com>
2025-10-03 13:23:46 -0300

    add etapa 3- função listar livros

--- main.py
+++ main.py
@@ -15,24 +15,43 @@ CREATE TABLE IF NOT EXISTS livros (
 """)
 
 
-def inserir_livros (Titulo,Autor,Ano):
+# def inserir_livros (Titulo,Autor,Ano):
+#     try:
+#         conexao = sqlite3.connect("Biblioteca.db")
+#         cursor = conexao.cursor()
+#         cursor.execute("""
+#         INSERT INTO livros (Titulo,Autor,Ano,Disponivel)
+#         VALUES (?,?,?,?)                   
+#         """,
+#         (Titulo,Autor,Ano,"sim")
+#         )
+#         conexao.commit()  
+#     except Exception as erro:
+#         print(f"erro ao inserir o livro {erro}")
+#     finally:
+#         if conexao:
+#             conexao.close()
+# Titulo  = input("Digite o Titulo do livro: ").lower()
+# Autor = input("Digite o Autor do livro: ").lower()
+# Ano = int(input("Digite a Ano do livro: "))
+# inserir_livros(Titulo,Autor,Ano)
+
+# print("-"*50)
+
+def listar_livros():
     try:
         conexao = sqlite3.connect("Biblioteca.db")
         cursor = conexao.cursor()
-        cursor.execute("""
-        INSERT INTO livros (Titulo,Autor,Ano,Disponivel)
-        VALUES (?,?,?,?)                   
-        """,
-        (Titulo,Autor,Ano,"sim")
-        )
-        conexao.commit()  
+        cursor.execute("SELECT * FROM livros")
+        for linha in cursor.fetchall():
+            print(f"id:{linha[0]}| Titulo: {linha[1]} |Autor:{linha[2]} | Ano:{linha[3]}| Dispolivel:{linha[4]}")
+
     except Exception as erro:
         print(f"erro ao inserir o livro {erro}")
     finally:
         if conexao:
             conexao.close()
-Titulo  = input("Digite o Titulo do livro: ").lower()
-Autor = input("Digite o Autor do livro: ").lower()
-Ano = int(input("Digite a Ano do livro: "))
-inserir_livros(Titulo,Autor,Ano)
+listar_livros()
+
+
 
